ca759a1a013582097df8bde5727b49cdcae52a2e
Johannes Konst <1047641+johanneskonst@users.noreply.github.com>
2020-10-31 16:39:15 +0100

    Adding a bit html, added composer

--- include/header.inc.php
+++ include/header.inc.php
@@ -2,16 +2,22 @@
 
 /**
  * I Can Speak At
- * 
+ *
  * Homepage
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/icanspeakat.css">
+        <link rel="icon" href="/assets/favicon.png" type="image/png">
+        <title>I Can Speak At</title>
     </head>
     <body>
